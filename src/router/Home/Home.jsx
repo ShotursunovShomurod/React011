723194b4c42fe8a7c2b6e26cb57b63e1e5cd51b9
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-08-22 21:05:33 +0500

    tugatilmagan

--- src/router/Home/Home.jsx
+++ src/router/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Featured from '../../components/Featured/Featured'
-import Stayhome from '../../components/StayHome/Stayhome'
+import Stayhome from '../../components/StayHome/Stayhome.jsx'
 import Collection from '../../components/Collection/Collection'
 
 const Home = () => {
